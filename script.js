15dc4989fe0eddccf12a019ca28d1b2acb769929
GwynethJoy17 <78654856+GwynethJoy17@users.noreply.github.com>
2022-02-22 20:26:00 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,84 @@
+
+    require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/Camera",
+      "esri/widgets/Home",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Camera, Home) {
+
+    
+      /*var map = new Map({
+        basemap: "streets",
+        ground: "world-elevation"
+      });*/
+      var scene = new WebScene({
+        portalItem:{
+      id:"b90bb9f8249a4d1b89c7820dcbe09e60" 
+        }
+      });
+      
+      var camera = new Camera({
+        position: [
+          -90.42874879718929, // lon
+          38.67258675072413, // lat
+          100000// elevation in meters
+        ],
+        tilt:0,
+        heading: 0
+      })
+      
+      var camera2 = new Camera({
+        position: [
+          -90.44345333970604, // lon
+          38.60144125537755, // lat
+          7000// elevation in meters
+        ],
+        tilt:0,
+        heading: 0
+      })
+
+      var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+        viewingMode:"global",
+        camera: camera,
+        environment: {
+            lighting: {
+              date: new Date(),
+              directShadowsEnabled: true,
+              // don't update the view time when user pans.
+              // The clock widget drives the time
+              cameraTrackingEnabled: false
+            }
+        },
+    });
+    
+    var homeBtn = new Home({
+        view: view
+      });
+
+      // Add the home button to the top left corner of the view
+    view.ui.add(homeBtn, "top-left");
+    
+    [stl, bei].forEach(function(button) {
+      button.style.display = 'flex';
+      view.ui.add(button, 'top-right');
+    });
+    
+    bei.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        target:camera2
+      });
+    });
+    
+    stl.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        target:camera
+      });
+    });
+
+
+    });
